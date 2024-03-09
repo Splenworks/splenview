90ffd8c0e9bab7f61f4fb4d2712d69c5da6ffd77
Royce Joe <roylory@gmail.com>
2024-03-09 15:17:46 -0800

    maximize image size without distorting ratio

--- src/ImageViewer.tsx
+++ src/ImageViewer.tsx
@@ -9,7 +9,11 @@ const ImageViewer: React.FC<ImageViewerProps> = ({ file }) => {
 
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center">
-      <img src={imageUrl} alt={imageUrl} className="max-w-full max-h-full" />
+      <img
+        src={imageUrl}
+        alt={imageUrl}
+        className="object-contain max-w-full max-h-full min-w-full min-h-full"
+      />
     </div>
   )
 }
