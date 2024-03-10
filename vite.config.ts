620f72235f8ee11fed5bf8bc781a5669318386fe
Royce Joe <roylory@gmail.com>
2024-03-09 20:10:42 -0800

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import svgr from "vite-plugin-svgr"
+import { VitePWA } from "vite-plugin-pwa"
 import { CommitHashPlugin } from "vite-plugin-commit-hash"
 
 // https://vitejs.dev/config/
@@ -9,6 +10,7 @@ export default defineConfig({
     react(),
     svgr(),
     CommitHashPlugin({ noPrefix: false, noVirtual: false }),
+    VitePWA({ registerType: "autoUpdate" }),
   ],
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
