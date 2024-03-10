d52ac95fa4ee78f3bf59cfc3c4cd819db37356c4
Royce Joe <roylory@gmail.com>
2024-03-09 16:04:02 -0800

    tab key to show file name

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { useEffect, useState } from "react"
 import DragDropArea from "./DragDropArea"
 import ImageViewer from "./ImageViewer"
+import FileInfo from "./FileInfo"
 
 function App() {
   const [files, setFiles] = useState<File[]>([])
   const [currentIndex, setCurrentIndex] = useState(0)
+  const [showFileInfo, setShowFileInfo] = useState(false)
 
   useEffect(() => {
+    const initialize = () => {
+      setFiles([])
+      setCurrentIndex(0)
+      setShowFileInfo(false)
+    }
+
     const handleKeyDown = (event: KeyboardEvent) => {
       if (files.length > 0) {
         if (
@@ -16,31 +24,40 @@ function App() {
         ) {
           if (currentIndex < files.length - 1) {
             setCurrentIndex((index) => index + 1)
+            setShowFileInfo(false)
           } else {
-            setFiles([])
-            setCurrentIndex(0)
+            initialize()
           }
         } else if (event.key === "ArrowLeft" || event.key === "ArrowUp") {
           if (currentIndex > 0) {
             setCurrentIndex((index) => index - 1)
+            setShowFileInfo(false)
           } else {
-            setFiles([])
-            setCurrentIndex(0)
+            initialize()
           }
+        } else if (event.key === "Tab") {
+          event.preventDefault()
+          setShowFileInfo((show) => !show)
         } else if (event.key === "Escape") {
-          setFiles([])
-          setCurrentIndex(0)
+          initialize()
         }
       }
     }
+
     window.addEventListener("keydown", handleKeyDown)
+
     return () => {
       window.removeEventListener("keydown", handleKeyDown)
     }
-  }, [files, setFiles, currentIndex, setCurrentIndex])
+  }, [files, setFiles, currentIndex, setCurrentIndex, setShowFileInfo])
 
   if (files.length > 0 && currentIndex < files.length) {
-    return <ImageViewer file={files[currentIndex]} />
+    return (
+      <>
+        <ImageViewer file={files[currentIndex]} />
+        {showFileInfo && <FileInfo fileName={files[currentIndex].name} />}
+      </>
+    )
   }
 
   return <DragDropArea setDroppedFiles={setFiles} />
---
+++ src/FileInfo.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+interface FileInfoProps {
+  fileName: string
+}
+
+const FileInfo: React.FC<FileInfoProps> = ({ fileName }) => {
+  return (
+    <div className="absolute top-5 right-5 opacity-50 bg-black p-2">
+      <span className="font-bold text-2xl text-white">{fileName}</span>
+    </div>
+  )
+}
+
+export default FileInfo
