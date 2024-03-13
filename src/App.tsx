c6f63d0ef1e068eec541b3b60c77f583887465cd
Royce Joe <roylory@gmail.com>
2024-03-12 19:55:48 -0700

    click to go next/prev

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,32 @@ function App() {
   const [currentIndex, setCurrentIndex] = useState(0)
   const [showFileInfo, setShowFileInfo] = useState(false)
 
-  useEffect(() => {
-    const initialize = () => {
-      setFiles([])
-      setCurrentIndex(0)
-      setShowFileInfo(false)
-    }
+  const initialize = () => {
+    setFiles([])
+    setCurrentIndex(0)
+    setShowFileInfo(false)
+  }
 
+  const goNext = () => {
+    if (currentIndex < files.length - 1) {
+      setCurrentIndex((index) => index + 1)
+      setShowFileInfo(false)
+    } else {
+      alert("You have reached the end of the list.")
+      initialize()
+    }
+  }
+
+  const goPrevious = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((index) => index - 1)
+      setShowFileInfo(false)
+    } else {
+      initialize()
+    }
+  }
+
+  useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (files.length > 0) {
         if (
@@ -23,20 +42,9 @@ function App() {
           event.key === "ArrowDown" ||
           event.key === " "
         ) {
-          if (currentIndex < files.length - 1) {
-            setCurrentIndex((index) => index + 1)
-            setShowFileInfo(false)
-          } else {
-            alert("You have reached the end of the list.")
-            initialize()
-          }
+          goNext()
         } else if (event.key === "ArrowLeft" || event.key === "ArrowUp") {
-          if (currentIndex > 0) {
-            setCurrentIndex((index) => index - 1)
-            setShowFileInfo(false)
-          } else {
-            initialize()
-          }
+          goPrevious()
         } else if (event.key === "Tab") {
           event.preventDefault()
           setShowFileInfo((show) => !show)
@@ -51,13 +59,21 @@ function App() {
     return () => {
       window.removeEventListener("keydown", handleKeyDown)
     }
-  }, [files, setFiles, currentIndex, setCurrentIndex, setShowFileInfo])
+  }, [files, currentIndex])
 
   if (files.length > 0 && currentIndex < files.length) {
     return (
       <>
         <ImageViewer file={files[currentIndex]} />
         {showFileInfo && <FileInfo fileName={files[currentIndex].name} />}
+        <div
+          className="fixed top-0 bottom-0 left-0 right-1/2 opacity-0"
+          onClick={goPrevious}
+        />
+        <div
+          className="fixed top-0 bottom-0 left-1/2 right-0 opacity-0"
+          onClick={goNext}
+        />
       </>
     )
   }
