096e0498bc347c62db6e78486050df1bf904c457
Royce Joe <roylory@gmail.com>
2024-03-29 11:19:59 -0700

    dark mode switch

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,13 +1,31 @@
 import React from "react"
 import CommitHash from "virtual:commit-hash"
+import { MoonIcon, SunIcon } from "@heroicons/react/16/solid"
 
 const Footer: React.FC = () => {
   return (
     <footer className="absolute bottom-0 left-0 right-0 bg-white dark:bg-neutral-900">
-      <div className="flex items-center justify-center h-16 mx-8 md:mx-16">
+      <div className="flex relative items-center justify-center h-16 mx-8 md:mx-16">
         <p className="text-black dark:text-white text-sm">
           Version {APP_VERSION}.{CommitHash.substring(0, 7)}
         </p>
+        <div className="absolute right-0">
+          <span
+            onClick={() => {
+              document.documentElement.classList.toggle("dark")
+              localStorage.setItem(
+                "theme",
+                document.documentElement.classList.contains("dark")
+                  ? "dark"
+                  : "light",
+              )
+            }}
+            className="flex group items-center justify-center rounded-full hover:bg-gray-200 cursor-pointer h-8 w-8"
+          >
+            <SunIcon className="hidden dark:block text-gray-300 group-hover:text-gray-600 h-4 w-4" />
+            <MoonIcon className="dark:hidden text-gray-400 group-hover:text-gray-600 h-4 w-4" />
+          </span>
+        </div>
       </div>
     </footer>
   )
