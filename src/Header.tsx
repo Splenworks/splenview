1e8de07adb4f1ed48a6d7fc440f36485a4575d3f
Royce Joe <roylory@gmail.com>
2024-03-30 12:33:40 -0700

    darkmode switch

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,15 @@
 import React from "react"
+import DarkModeSwitch from "./DarkModeSwitch"
 
 const Header: React.FC = () => {
   return (
     <header className="absolute top-0 left-0 right-0 bg-white dark:bg-neutral-900">
       <div className="flex items-center justify-center h-16 px-16 text-lg font-semibold">
+        <div className="flex-1"></div>
         <p className="text-black dark:text-white">SplenView</p>
+        <div className="flex-1 flex justify-end items-center">
+          <DarkModeSwitch />
+        </div>
       </div>
     </header>
   )
