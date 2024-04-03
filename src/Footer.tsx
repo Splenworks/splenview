fffc9703aa2225ec88fe3de7d854ee1ab0a66d75
Royce Joe <roylory@gmail.com>
2024-04-03 22:17:41 +0100

    Hide certain footer element on small screens

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="absolute bottom-0 left-0 right-0 bg-white dark:bg-neutral-900">
       <div className="flex items-center justify-center h-16 mx-8 md:mx-16">
-        <div className="flex-1"></div>
+        <div className="flex-1 hidden sm:block"></div>
         <p className="text-black dark:text-white text-sm">
           Check out our other product{" "}
           <a
@@ -16,7 +16,7 @@ const Footer: React.FC = () => {
             SplenPlayer
           </a>
         </p>
-        <div className="flex-1 text-sm text-right text-white dark:text-neutral-900">
+        <div className="flex-1 text-sm text-right text-white dark:text-neutral-900 hidden sm:block">
           Version {APP_VERSION}.{CommitHash.substring(0, 7)}
         </div>
       </div>
