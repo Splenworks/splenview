ea472f465fa2a5d15f2011b9740efe735f70d0c9
Royce Joe <roylory@gmail.com>
2024-04-21 02:03:22 -0700

    go back button in translation

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import DarkModeSwitch from "./DarkModeSwitch"
 import { ArrowUturnLeftIcon } from "@heroicons/react/16/solid"
+import { useTranslation } from "react-i18next"
 
 interface HeaderProps {
   exited: boolean
@@ -8,20 +9,21 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ exited, goBack }) => {
+  const { t } = useTranslation()
   return (
     <header className="absolute top-0 left-0 right-0 bg-white dark:bg-neutral-900">
       <div className="flex items-center justify-center h-16 mx-8 md:mx-16">
-        <div
-          className="flex-1 flex items-center gap-2 cursor-pointer"
-          onClick={() => exited && goBack()}
-        >
+        <div className="flex-1 flex">
           {exited && (
-            <>
+            <div
+              className="flex items-center cursor-pointer gap-2"
+              onClick={goBack}
+            >
               <ArrowUturnLeftIcon className="h-5 w-5 text-black dark:text-white" />
               <span className="text-black dark:text-white text-md font-semibold">
-                Go Back
+                {t("header.goBack")}
               </span>
-            </>
+            </div>
           )}
         </div>
         <p className="text-black dark:text-white text-lg font-semibold">
