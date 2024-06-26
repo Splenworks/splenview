3c0d536def21f55a22684540e348c820839f22d0
Royce Joe <roylory@gmail.com>
2024-06-26 10:50:57 -0700

    In tooltip comp, use hover style instead of mouse events

--- src/Tooltip.tsx
+++ src/Tooltip.tsx
@@ -13,49 +13,39 @@ const Tooltip: React.FC<PropsWithChildren<TooltipProps>> = ({
   place = "top",
   align = "center",
 }) => {
-  const [showTooltip, setShowTooltip] = useState(false)
-
   if (!text) {
     return <>{children}</>
   }
 
   return (
-    <div
-      onMouseEnter={() => setShowTooltip(true)}
-      onMouseLeave={() => setShowTooltip(false)}
-      className="relative cursor-pointer"
-    >
+    <div className="relative cursor-pointer group">
       {children}
-      {showTooltip && (
-        <>
-          <div
-            className={twJoin(
-              "absolute transform",
-              place === "top"
-                ? "bottom-full -translate-y-2"
-                : "top-full translate-y-2",
-              align === "left"
-                ? "left-0"
-                : align === "right"
-                  ? "right-0"
-                  : "left-1/2 -translate-x-1/2",
-            )}
-            style={{ width: "max-content" }}
-          >
-            <div className="px-2 py-1 bg-zinc-500 rounded bg-opacity-50">
-              <p className="font-normal text-xs text-white">{text}</p>
-            </div>
-          </div>
-          <div
-            className={twJoin(
-              "absolute border-solid border-opacity-50 border-x-transparent border-x-[6px] left-1/2 transform -translate-x-1/2",
-              place === "bottom"
-                ? "border-b-zinc-500 border-b-[6px] border-t-0 top-full translate-y-[2px]"
-                : "border-t-zinc-500 border-t-[6px] border-b-0 bottom-full -translate-y-[2px]",
-            )}
-          ></div>
-        </>
-      )}
+      <div
+        className={twJoin(
+          "absolute transform hidden group-hover:block transition-transform duration-300 ease-in-out",
+          place === "top"
+            ? "bottom-full -translate-y-2"
+            : "top-full translate-y-2",
+          align === "left"
+            ? "left-0"
+            : align === "right"
+              ? "right-0"
+              : "left-1/2 -translate-x-1/2",
+        )}
+        style={{ width: "max-content" }}
+      >
+        <div className="px-2 py-1 bg-zinc-500 rounded bg-opacity-50">
+          <p className="font-normal text-xs text-white">{text}</p>
+        </div>
+      </div>
+      <div
+        className={twJoin(
+          "absolute border-solid border-opacity-50 border-x-transparent border-x-[6px] left-1/2 transform -translate-x-1/2 hidden group-hover:block transition-transform duration-300 ease-in-out",
+          place === "bottom"
+            ? "border-b-zinc-500 border-b-[6px] border-t-0 top-full translate-y-[2px]"
+            : "border-t-zinc-500 border-t-[6px] border-b-0 bottom-full -translate-y-[2px]",
+        )}
+      ></div>
     </div>
   )
 }
