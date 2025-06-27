d12dc3eeab3d193d52c4950551eb0f276b123044
Royce Joe <roylory@gmail.com>
2025-06-27 16:18:27 -0700

    feat: revoke object URL when file changes

--- src/ImageViewer.tsx
+++ src/ImageViewer.tsx
@@ -1,11 +1,17 @@
-import React from "react"
+import React, { useEffect, useMemo } from "react"
 
 interface ImageViewerProps {
   file: File
 }
 
 const ImageViewer: React.FC<ImageViewerProps> = ({ file }) => {
-  const imageUrl = URL.createObjectURL(file)
+  const imageUrl = useMemo(() => URL.createObjectURL(file), [file])
+
+  useEffect(() => {
+    return () => {
+      URL.revokeObjectURL(imageUrl)
+    }
+  }, [imageUrl])
 
   return (
     <div className="fixed bottom-0 left-0 right-0 top-0 flex items-center justify-center">
