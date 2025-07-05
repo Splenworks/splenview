bde51e2187f5d7c53f86565f466efb2b12b60134
Royce Joe <roylory@gmail.com>
2025-07-05 16:42:14 -0700

    fix file not found issue in dev mode

--- src/ImageViewer.tsx
+++ src/ImageViewer.tsx
@@ -1,17 +1,22 @@
-import React, { useEffect, useMemo } from "react"
+import React, { useEffect, useState } from "react"
 
 interface ImageViewerProps {
   file: File
 }
 
 const ImageViewer: React.FC<ImageViewerProps> = ({ file }) => {
-  const imageUrl = useMemo(() => URL.createObjectURL(file), [file])
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
 
   useEffect(() => {
+    const objectUrl = URL.createObjectURL(file);
+    setImageUrl(objectUrl);
+
     return () => {
-      URL.revokeObjectURL(imageUrl)
-    }
-  }, [imageUrl])
+      URL.revokeObjectURL(objectUrl);
+    };
+  }, [file]);
+
+  if (!imageUrl) return null;
 
   return (
     <div className="fixed bottom-0 left-0 right-0 top-0 flex items-center justify-center">
